4c1b052b60aa34f6e60caf3b56a7b691ba17b5e4
DBAAZzz <chd.zhong@muheda.com>
2022-03-09 20:10:30 +0800

    commit

---
+++ uploadSourceMap.js
@@ -0,0 +1,60 @@
+const { readdir, rm } = require('fs/promises')
+const { createReadStream } = require('fs')
+const axios = require('axios')
+const FormData = require('form-data')
+const path = require('path')
+
+const PLUGINS_NAME = 'UploadSourceMapPlugin'
+
+class UploadSourceMapPlugin {
+
+    constructor(options) {
+        console.log('options', options)
+    }
+    
+    async getAssets(distDir) {
+        try {
+            // 获取所有sourceMap的文件名
+            const files = await readdir(distDir)
+            // 获取所有souceMap的文件路径
+            return files.filter(el => /\.js\.map$/i.test(el)).map(el => path.join(distDir, el))
+        } catch (error) {
+            console.log('error', error)
+        }
+    }
+
+    async uploadFile(filePath) {
+        const stream = createReadStream(filePath)
+        const formData = new FormData()
+        formData.append('file', stream)
+        return axios.default({
+            url: 'http://127.0.0.1:3000/file/upload',
+            method: 'post',
+            headers: {
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjA5MjljNjQwLTg3OWQtMTFlYi1iNGExLTMzM2ZhNTQ5ZjVlYSIsImlhdCI6MTYzODUwMjcxNywiZXhwIjoxNjM4NTg5MTE3fQ.oeq1nXIeEpvpMQkutFuaJSUkx0FeU0K0ES4Iz9toN8w',
+                ...formData.getHeaders(),
+            },
+            timeout: 10000,
+            data: formData
+        }).then((res) => {
+            console.log('uploadResponse', res)
+        }).catch((err) => {
+            console.log('uploadError', err)
+        })
+    }
+
+    apply(compiler) {
+        const sourceMapDir = path.join(compiler.options.output.path, 'sourcemap')
+        compiler.hooks.afterEmit.tapPromise(PLUGINS_NAME, async () => {
+            let files = await this.getAssets(path.join(sourceMapDir, 'js'))
+            for (const file of files) {
+                await this.uploadFile(file)
+            }
+            await rm(sourceMapDir, {
+                recursive: true
+            })
+        })
+    }
+}
+
+module.exports = UploadSourceMapPlugin;
